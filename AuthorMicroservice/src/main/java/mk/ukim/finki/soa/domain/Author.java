ef67bf92432399bd6699bc1cc3ff6b02a00081ba
kirkovg <gjorgjikirkov@gmail.com>
2017-12-10 19:39:32 +0000

    Fixed crashing of author

--- AuthorMicroservice/src/main/java/mk/ukim/finki/soa/domain/Author.java
+++ AuthorMicroservice/src/main/java/mk/ukim/finki/soa/domain/Author.java
@@ -1,7 +1,5 @@
 package mk.ukim.finki.soa.domain;
 
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -12,7 +10,6 @@ import java.util.Objects;
  */
 @Entity
 @Table(name = "author")
-@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Author implements Serializable {
 
     private static final long serialVersionUID = 1L;
